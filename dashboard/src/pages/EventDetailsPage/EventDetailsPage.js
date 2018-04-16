bd96cfd263b819bc3ad9456998fe12d2f580f114
James Phillips <jamesdphillips@gmail.com>
2018-04-16 00:18:57 -0700

    [WIP] Check Results
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- dashboard/src/pages/EventDetailsPage/EventDetailsPage.js
+++ dashboard/src/pages/EventDetailsPage/EventDetailsPage.js
@@ -1,32 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { graphql } from "react-relay";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const query = gql`
+  query EventDetailsPageQuery(
+    $ns: NamespaceInput!
+    $check: String!
+    $entity: String!
+  ) {
+    event(ns: $ns, entity: $entity, check: $check) {
+      id
+      timestamp
+    }
+  }
+`;
 
 class EventDetailsPage extends React.Component {
   static propTypes = {
     match: PropTypes.object.isRequired,
   };
 
-  static query = graphql`
-    query EventDetailsPageQuery(
-      $ns: NamespaceInput!
-      $check: String!
-      $entity: String!
-    ) {
-      event(ns: $ns, entity: $entity, check: $check) {
-        id
-        timestamp
-      }
-    }
-  `;
-
   render() {
     const { match } = this.props;
+    const ns = {
+      organization: match.params.organization,
+      environment: match.params.environment,
+    };
+
     return (
-      <React.Fragment>
-        <p>entity: {match.params.entity}</p>
-        <p>check: {match.params.check}</p>
-      </React.Fragment>
+      <Query query={query} variables={{ ...match.params, ns }}>
+        {({ data = {}, loading }) => {
+          if (loading) return <div>Loading...</div>;
+
+          return (
+            <React.Fragment>
+              <p>timestamp: {data.event.timestamp}</p>
+            </React.Fragment>
+          );
+        }}
+      </Query>
     );
   }
 }
+
+export default EventDetailsPage;
---
+++ dashboard/src/pages/EventDetailsPage/EventDetailsPageCheckResult.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import gql from "graphql-tag";
+
+class EventDetailsCheckResult extends React.Component {
+  static propTypes = {
+    check: PropTypes.object.isRequired,
+  };
+
+  static fragments = {
+    check: gql`
+      fragment EventDetailsCheckResult_check on Check {
+        status
+        lastOk
+      }
+    `,
+  };
+
+  render() {
+    const { check } = this.props;
+
+    return (
+      <React.Fragment>
+        <p>status: {check.status}</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default EventDetailsCheckResult;
